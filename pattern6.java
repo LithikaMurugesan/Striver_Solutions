da3d18ab23cea674c4f4c858dad5267d9cd1e79f
Lithika Murugesan <lithika2404@gmail.com>
2026-01-01 12:31:40 +0530

    pattern6.java

---
+++ pattern6.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public  class striver {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1;i<=n;i++){
+            for(int j = n ;j>=i;j--){
+                System.out.print(n-j+1);
+            }
+            System.out.println(" ");
+        }
+    }
+}
