88aa44af2dd85d61e6d7c209905879e40ec4d283
Lithika Murugesan <lithika2404@gmail.com>
2026-01-01 07:10:26 +0530

    pattern2.java

---
+++ pattern2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class striver {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int a =0;a<n;a++){
+            for(int b = 0 ;b<=a;b++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+
+}
