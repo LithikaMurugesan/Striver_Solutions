7226baf0feaea0215f3867ba3a829e47590cdceb
Lithika Murugesan <lithika2404@gmail.com>
2026-01-02 11:25:18 +0530

    pattern8.java

---
+++ pattern8.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public  class striver {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i =0;i<n;i++){
+            for(int j = 0 ; j<i;j++){
+                System.out.print("s");
+            }for(int j = 0;j<2*n-(2*i+1);j++){
+                System.out.print("*");
+            }for(int j = 0;j<i;j++){
+                System.out.print("s");
+            }
+        }
+        System.out.println();
+    }
+}
