a943d2ad6c5bd4df197064cb7ea3538bfb532010
Lithika Murugesan <lithika2404@gmail.com>
2026-01-05 16:12:46 +0530

    pattern18.java

---
+++ pattern18.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class striver {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i =0;i<=n;i++){
+            for(char j = (char) ('A'+n-i); j<='A'+n-1; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
