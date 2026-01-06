bcb1177be04fcdc52ed677926128051bddb0df41
Lithika Murugesan <lithika2404@gmail.com>
2026-01-06 15:55:29 +0530

    Rotate Array by one left.java

---
+++ Rotate Array by one left.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class striver {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+      String[] str = s.split(" ");
+      int[] arr = new int[str.length];
+      for(int i =0;i<str.length;i++){
+          arr[i]=Integer.parseInt(str[i]);
+      }
+
+        System.out.println(Arrays.toString(Rotate(arr)));
+    }
+    static int[] Rotate(int[] arr){
+        int first = arr[0];
+        for(int i =1;i<arr.length;i++){
+
+
+        arr[i-1]=arr[i];
+
+        }
+        arr[arr.length-1]=first;
+        return arr;
+    }
+}
