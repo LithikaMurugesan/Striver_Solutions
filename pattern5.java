da9b5ec2f18c10c89043c44aa3bf84b4773d127b
Lithika Murugesan <lithika2404@gmail.com>
2026-04-29 06:12:43 +0530

    Pattern5.java

--- pattern5.java
+++ pattern5.java
@@ -1,13 +1,14 @@
 import java.util.*;
-public class Learn {
+public class solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i =1;i<=n;i++){
-            for(int j =n;j>=i;j--){
-                System.out.print("*");
+        for(int i =0;i<n;i++){
+            for(int j=n;j>=i+1;j--){
+                System.out.print("*"+" ");
             }
             System.out.println();
         }
+
     }
 }
